24a5ffd1ebd83d3d9d7c797cb0d2244d9f914ed5
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-01 22:57:51 -0700

    initial

---
+++ 1.5/Source/PipiruPiruPiruPipiruPii/HarmonyInit.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PipiruPiruPiruPipiruPii
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            Harmony harmonyInstance = new Harmony("BBLKepling.PipiruPiruPiruPipiruPii");
+            harmonyInstance.Patch(typeof(ResurrectionUtility).GetMethod("TryResurrect"), postfix: new HarmonyMethod(typeof(ResurrectionUtilityPatches).GetMethod("Postfix")));
+            harmonyInstance.Patch(typeof(ResurrectionUtility).GetMethod("TryResurrectWithSideEffects"), postfix: new HarmonyMethod(typeof(ResurrectionUtilityPatches).GetMethod("Postfix")));
+        }
+    }
+}
---
+++ 1.5/Source/PipiruPiruPiruPipiruPii/InternalDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace PipiruPiruPiruPipiruPii
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+        public static SoundDef BBLK_PipiruPiruPiruPipiruPii;
+    }
+}
