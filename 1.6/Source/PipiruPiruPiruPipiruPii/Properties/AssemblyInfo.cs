60c3634365d715f9d7f73935930a72b77e4b0129
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-11 22:11:02 -0700

    1.6 update

---
+++ 1.6/Source/PipiruPiruPiruPipiruPii/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PipiruPiruPiruPipiruPii")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PipiruPiruPiruPipiruPii")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cde4062a-edca-4ca0-9d1e-0665feffed58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 1.6/Source/PipiruPiruPiruPipiruPii/ResurrectionUtilityPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse.Sound;
+using Verse;
+
+namespace PipiruPiruPiruPipiruPii
+{
+    public class ResurrectionUtilityPatches
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn, ref bool __result)
+        {
+            if (__result == true) InternalDefOf.BBLK_PipiruPiruPiruPipiruPii.PlayOneShot(new TargetInfo(pawn.Position, pawn.Map));
+        }
+    }
+}
